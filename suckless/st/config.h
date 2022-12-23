2ed8d36831343bd8027438b6ad255a84d17b2653
Agung Satria <agungsatria890@gmail.com>
2022-12-23 07:05:28 +0700

    fix dashboard size

--- suckless/st/config.h
+++ suckless/st/config.h
@@ -119,7 +119,8 @@ unsigned int tabspaces = 8;
 
 /* bg opacity */
 // float alpha = 1.0;
-float alpha = 0.90;
+// float alpha = 0.90;
+float alpha = 0.85;
 //float alpha = 0.75;
 
 /* Terminal colors (16 first used in escape sequence) */
